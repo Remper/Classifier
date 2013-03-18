268d0b21691b703861bc94711a260d03dad5aa23
Yaroslav Nechaev <remper@me.com>
2013-03-18 14:35:41 +0400

    Заготовка под парсер

---
+++ Instruments/parseFilms.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Parse films from trecs2011
+ *
+ * @author Yaroslav Nechaev <remper@me.com>
+ */
+
+include("../bootstrap.php");
+
+use Tokenizer\Tokenizer;
+use Tokenizer\Mystem;
+use Tokenizer\Solver;
+use Tokenizer\Database;
+use Tokenizer\Sentence;
+
+error_reporting(E_ALL);
+
+//Замеряем начальное время
+$start_time = microtime(true);
+
+//Открыть лог
+$config = parse_ini_file("../settings.ini", true);
+$log = new \Log(\LogType::LEARNER, ".." . $config['log']['dir'], true);
+
+$log->writeLog("System ready, starting Learner");
+
+//Основной тест
+$tokenizer = new Tokenizer($config);
+
+
+$log->writeLog("Done in: " . number_format(microtime(true) - $start_time, 4, ".", " ") . " seconds");
